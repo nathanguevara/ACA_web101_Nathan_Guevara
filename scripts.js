3d32c197cf9b5f4db5a2374730a91b8df481199d
nathanguevara <nathan.guevara@gmail.com>
2022-02-20 09:20:57 -0600

    fixed divs

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 const placeForResults = document.getElementById("results")
-
+console.log("this is the element",placeForResults)
 new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
